7031a2f9ae8a6b13a420c6e22fdb402f04f9d2d8
Vera Weikel <vera.weikel@gmail.com>
2019-08-23 16:10:22 -0700

    Update js files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 /* 
-Spec #1: The program returns a range of numbers from 0 to the users inputted number
+Spec : Input a number
+Spec #: The program returns a range of numbers from 0 to the users inputted number
 Input: "4"
 Output: "0, 1, 2, 3, 4"
 */
@@ -9,41 +10,14 @@ $(document).ready(function(){
     console.log("fxn-inner");
     let inputNumber = parseInt($("#input-number").val());//spec#1
     console.log("Submit button Event function: Input",inputNumber);
-    // let i;
-    // let iLoopNumbers = [i];
-    // inputNumber.forEach(function(iLoopNumbers){
-    //     iLoopNumbers.push(inputNumS + "," + inputNumber);
-    // });
-    // if (inputNumber > 0) {
-    //     return inputNumber
-    // }
 
-        for (let count = 0; count <= inputNumber.length; count ++){
+        for (let count = 0; ; count ++){
+            console.log("Inside for loop!", count + inputNumber);
                 if (count === inputNumber);
-                console.log("Inside for loop!", count + inputNumber);
-                return count;
-                //return inputNumber + count; //this works to return input only; 
+                return inputNumber + count; //this works; 
                 //why did this not work: return inputNumber(count);
-        }
-  
-    // try 57: for (let i = inputNumber; i <= inputNumber.length; i-- ) { //inputNumberReturns
-    // // // let i = inputNumber;
-    //     console.log(inputNumber[i]);
-
-    // }};
-    // for ( i = 0; i < inputNumber.length;) {
-    //     console.log("inside loop:", inputNumber.i);
-    //     text += inputNumber[i++];
-    // }};
-
-        // Attempting to dynamically populate the .html output:
-        // let inputsAndOutputs = ["input-number","input-number-range-return"];
-        // inputsAndOutputs.forEach(inputAndOutput => {
-        //     let generatedIntegers = 
-        // });
-        // $("#result").text(inputsAndOutputs);
-
-       // $("#result").text();
+              }
+        return result = $("#result").text(inputNumber);
         event.preventDefault();
     });
     console.log("fxn-outer");
